f7af01e0961b5627b6ac7efbe362cb493950a782
Binh <ntcbinh16@gmail.com>
2021-12-16 17:46:22 +0700

    test commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Binh.n',
-    title: 'todo-list',
+    title: 'vue-todo',
     htmlAttrs: {
       lang: 'en'
     },
